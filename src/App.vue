dfb2c17f588dbff20c30a646928e19eb4f3e498a
arcbjorn <arky.happ@gmail.com>
2021-04-19 19:58:26 +0300

    feat (src): clean up the project from boilerplate

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <Dashboard msg="Arc Crypto Monitor" />
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import Dashboard from "./components/Dashboard.vue";
 
 export default defineComponent({
   name: "App",
   components: {
-    HelloWorld,
+    Dashboard,
   },
 });
 </script>
@@ -22,6 +21,6 @@ export default defineComponent({
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 5vh;
 }
 </style>
---
+++ src/components/Dashboard.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+    <p>Simple crypto coin monitoring solution for fellow geeks ☺️ 🤖</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Dashboard",
+  props: {
+    msg: String,
+  },
+});
+</script>
+
+<style scoped></style>
