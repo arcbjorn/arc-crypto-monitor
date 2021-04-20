adbfa523f71efca3a7542170c8542404e273997d
arcbjorn <arky.happ@gmail.com>
2021-04-20 17:30:59 +0300

    reafactor (api): devide subscriptions into fns

--- src/api/subscriptions.ts
+++ src/api/subscriptions.ts
@@ -3,24 +3,62 @@ import subsArray from "./subsHelper";
 const apiKey =
   "35c7ef71c64f11a95f1b0ee0c58f6218af91236d8c30bd3133f9a30811dc00a3";
 
-const openConnection = (): void => {
-  const connection = new WebSocket(
-    "wss://streamer.cryptocompare.com/v2?api_key=" + apiKey
-  );
+export const openConnection = (): WebSocket => {
+  try {
+    const connection = new WebSocket(
+      "wss://streamer.cryptocompare.com/v2?api_key=" + apiKey
+    );
 
-  connection.onopen = function (): void {
-    console.log("Connected! :)");
+    connection.onopen = function (): void {
+      console.log("Successfully connected to the CryptoCompare server...");
+      const subRequest = {
+        action: "SubAdd",
+        subs: subsArray ?? ["2~Coinbase~BTC~USD"],
+      };
+      connection.send(JSON.stringify(subRequest));
+    };
+
+    connection.onmessage = function (event: MessageEvent): void {
+      const message = event.data;
+      console.log("Received from Cryptocompare: " + message);
+    };
+
+    return connection;
+  } catch (error) {
+    console.log(error);
+    return error.message;
+  }
+};
+
+export const closeConnection = (ws: WebSocket): boolean => {
+  try {
+    ws.close();
+    console.log("Successfully closed connection to the CryptoCompare.");
+
+    return true;
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
+};
+
+export const unsubscribeFromAllTickers = (ws: WebSocket): boolean => {
+  try {
     const subRequest = {
-      action: "SubAdd",
+      action: "SubRemove",
       subs: subsArray ?? ["2~Coinbase~BTC~USD"],
     };
-    connection.send(JSON.stringify(subRequest));
-  };
+    ws.send(JSON.stringify(subRequest));
 
-  connection.onmessage = function (event: MessageEvent): void {
-    const message = event.data;
-    console.log("Received from Cryptocompare: " + message);
-  };
+    console.log(
+      "Successfully unsubscribed from all tickers on CryptoCompare server."
+    );
+
+    return true;
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
 };
 
 export default openConnection;
