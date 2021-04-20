34f4772564c895e305dc950872f171861bda68de
arcbjorn <arky.happ@gmail.com>
2021-04-20 19:11:48 +0300

    build (Pre-Processors): add Pug to Webpack

--- vue.config.js
+++ vue.config.js
@@ -4,5 +4,11 @@ module.exports = {
       args[0].title = "Arc Crypto Monitor";
       return args;
     });
+    config.module
+      .rule("pug")
+      .test(/\.pug$/)
+      .use("pug-plain-loader")
+      .loader("pug-plain-loader")
+      .end();
   },
 };
