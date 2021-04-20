a5a869915b73c5fb6f885873b793aa52fc55519d
arcbjorn <arky.happ@gmail.com>
2021-04-20 23:15:45 +0300

    feat (src): add Table component to Dashboard

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,17 +1,22 @@
-<template>
-  <div>
-    <h1>{{ msg }}</h1>
-    <p>Simple crypto coin monitoring solution for fellow geeks ☺️ 🤖</p>
-  </div>
+<template lang="pug">
+.container.mx-auto.px-4
+  .font-bold.text-4xl.p-2 {{ msg }}
+  .block.bg-green-200.p-4.my-6.rounded-md.text-md
+    | Simple crypto coin monitoring solution for fellow geeks ☺️ 🤖
+  Table
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import Table from "@/components/Table.vue";
 import { useStore } from "@/store";
 // import openConnection from "@/api/subscriptions";
 
 export default defineComponent({
   name: "Dashboard",
+  components: {
+    Table,
+  },
   props: {
     msg: String,
   },
---
+++ src/components/Table.vue
@@ -0,0 +1,43 @@
+<template lang="pug">
+table.mx-auto.table-fixed.border-separate.border.border-green-800.rounded-md(class="min-w-3/4")
+  thead
+    tr
+      th.py-2.text-xl(class="w-1/2") Coin
+      th.py-2.text-xl(class="w-1/2") Value
+  tbody
+    tr
+      td.border.border-green-600.rounded-md.py-2 BTC
+      td.border.border-green-600.rounded-md.py-2 50.000 USD
+    tr
+      td.border.border-green-600.rounded-md.py-2 ETH
+      td.border.border-green-600.rounded-md.py-2 25.000 USD
+    tr
+      td.border.border-green-600.rounded-md.py-2 DOGE
+      td.border.border-green-600.rounded-md.py-2 15.000 USD
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { useStore } from "@/store";
+// import openConnection from "@/api/subscriptions";
+
+export default defineComponent({
+  name: "Table",
+  // components: {
+  //   Table,
+  // },
+  props: {
+    msg: String,
+  },
+  setup: () => {
+    const { state } = useStore();
+
+    console.log(state.tickers);
+  },
+  created: function () {
+    // openConnection();
+  },
+});
+</script>
+
+<style scoped></style>
