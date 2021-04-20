593f9788f1ec4b69a322f463476940fbd249797c
arcbjorn <arky.happ@gmail.com>
2021-04-20 23:25:28 +0300

    test (e2e): update view-dashboard test

--- tests/e2e/specs/view-dashboard.spec.js
+++ tests/e2e/specs/view-dashboard.spec.js
@@ -3,6 +3,6 @@
 describe("Render the page", () => {
   it("Visits the app root url", () => {
     cy.visit("/");
-    cy.contains("h1", "Arc Crypto Monitor");
+    cy.contains("div", "Arc Crypto Monitor").should("be.visible");
   });
 });
