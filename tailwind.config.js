206467288eccf802a94a8a9b864a0a517e091f44
arcbjorn <arky.happ@gmail.com>
2021-04-23 23:41:46 +0300

    styles (Table): extend class with hight

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
+      height: {
+        80: "80%",
+      },
       width: {
         half: "50%",
       },
