e59178bd155d26faf75d3f845ad0bbfd77108b87
arcbjorn <arky.happ@gmail.com>
2021-04-24 17:36:23 +0300

    feat (Table): add functional search with icon

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,38 +1,52 @@
 <template lang="pug">
 div
-  .button-group
-    button(:class="getActiveButton(CurrencyType.EUR)" @click="setCurrency(CurrencyType.EUR)") EUR
-    button(:class="getActiveButton(CurrencyType.USD)" @click="setCurrency(CurrencyType.USD)") USD
-    Button(:class="getActiveButton(CurrencyType.RUB)" @click="setCurrency(CurrencyType.RUB)") RUB
+  .table-header
+    div
+      .search-box
+        Icon(name="search").ml-4
+        input.search-input(v-model="searchData" @input="setSearch" type="text" placeholder="Search")
+    .button-group
+      button(:class="getActiveButton(CurrencyType.EUR)" @click="setCurrency(CurrencyType.EUR)") EUR
+      button(:class="getActiveButton(CurrencyType.USD)" @click="setCurrency(CurrencyType.USD)") USD
+      button(:class="getActiveButton(CurrencyType.RUB)" @click="setCurrency(CurrencyType.RUB)") RUB
   table
     thead
       tr
         th Coin
         th Value
     tbody
-      tr(v-for="coin, i in Object.values(coins)" :key="i")
+      tr(v-for="coin, i in Object.values(getCoinsByName)" :key="i")
         td {{ coin.name }}
         td {{ getPriceByCurrency(coin) }}
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import { mapActions, mapState } from "vuex";
+import Icon from "@/components/Icon.vue";
+import { mapActions, mapGetters } from "vuex";
 import { Coin, CurrencyType } from "@/types";
 
 export default defineComponent({
   name: "Table",
+  components: {
+    Icon,
+  },
   data: () => {
     return {
       currency: CurrencyType.USD,
+      searchData: "",
       CurrencyType: CurrencyType,
     };
   },
   computed: {
-    ...mapState(["coins", "usdToRub"]),
+    ...mapGetters(["getCoinsByName"]),
   },
   methods: {
-    ...mapActions(["getUsdToRubRate", "openTickerSubConnection"]),
+    ...mapActions([
+      "getUsdToRubRate",
+      "openTickerSubConnection",
+      "updateSearchData",
+    ]),
 
     getPriceByCurrency(coin: Coin): string {
       return `${coin[this.currency]} ${this.currency}`;
@@ -45,6 +59,10 @@ export default defineComponent({
 
     setCurrency(btnType: CurrencyType): void {
       this.currency = btnType;
+    },
+
+    setSearch() {
+      this.updateSearchData(this.searchData);
     },
   },
   beforeMount: function () {
